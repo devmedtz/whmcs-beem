d7c758824ae5451c41eb93d5f12dabe841623309
medson <medsonnaftal@gmail.com>
2021-07-17 07:35:44 +0300

    final

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# whmcs-beem
+* INSTALLATION INSTRUCTIONS BEEM WHMCS V1.1.* *
+
+The Beem gateway module requires the use of modified templates that are provided with this distribution. 
+
+Please follow the instructions below to setup the Beem gateway module. 
+
+
+
+The templates provided are based on WHMCS 7.1.X and the default theme.
+
+* 
+
+Upload beem.php AND the beem folder to your whmcs's modules/gateways folder.
+
+* 
+
+Upload the callback/beem.php file to your whmcs's modules/gateways/callback folder.
+
+* 
+
+
+These templates are based off of the default template in 7.1.2.* 
+
+Enable the Beem module in the WHMCS admin area by going to Setup->Payments->Payment Gateways->Beem and paste in your administrator API KEY, SECRET KEY. 
+
+* 
+
+Ensure when you are done testing the plugin using the demo/sandbox account you switch to the live API.
+
+*
+
+Save configurations.
+
+* 
+
+If you have any questions, recommendations or need installation assistance, please send us an email to medsonnaftal@gmail.com
+    
--- callback/beem.php
+++ callback/beem.php
@@ -1,18 +1,6 @@
 <?php
 /**
  * WHMCS Sample Payment Callback File
- *
- * This sample file demonstrates how a payment gateway callback should be
- * handled within WHMCS.
- *
- * It demonstrates verifying that the payment gateway module is active,
- * validating an Invoice ID, checking for the existence of a Transaction ID,
- * Logging the Transaction for debugging and Adding Payment to an Invoice.
- *
- * For more information, please refer to the online documentation.
- *
- * @see https://developers.whmcs.com/payment-gateways/callbacks/
- *
  * @copyright Copyright (c) WHMCS Limited 2017
  * @license http://www.whmcs.com/license/ WHMCS Eula
  */
@@ -21,6 +9,7 @@
 require_once __DIR__ . '/../../../init.php';
 require_once __DIR__ . '/../../../includes/gatewayfunctions.php';
 require_once __DIR__ . '/../../../includes/invoicefunctions.php';
+global $CONFIG;
 
 // Detect module name from filename.
 $gatewayModuleName = basename(__FILE__, '.php');
@@ -30,87 +19,34 @@ $gatewayParams = getGatewayVariables($gatewayModuleName);
 
 // Die if module is not active.
 if (!$gatewayParams['type']) {
-    die("Module Not Activated");
+    die("Beem Module Not Activated");
 }
 
 // Retrieve data returned in payment gateway callback
-// Varies per payment gateway
-$success = $_POST["x_status"];
-$invoiceId = $_POST["x_invoice_id"];
-$transactionId = $_POST["x_trans_id"];
-$paymentAmount = $_POST["x_amount"];
-$paymentFee = $_POST["x_fee"];
-$hash = $_POST["x_hash"];
+$paymentAmount = $_GET["amount"];
+$paymentFee = 0.0;
+$refrenceNumber = $_GET["refrenceNumber"];
+$status = $_GET["status"];
+$timestamp = $_GET["timestamp"];
+$transactionId = $_GET["transactionId"];
+$msisdn = $_GET["msisdn"];
 
-$transactionStatus = $success ? 'Success' : 'Failure';
+$keyword = $gatewayParams['keyword'];
+$invoiceId = trim($refrenceNumber, "'$keyword.'-");
 
-/**
- * Validate callback authenticity.
- *
- * Most payment gateways provide a method of verifying that a callback
- * originated from them. In the case of our example here, this is achieved by
- * way of a shared secret which is used to build and compare a hash.
- */
-$secretKey = $gatewayParams['secretKey'];
-if ($hash != md5($invoiceId . $transactionId . $paymentAmount . $secretKey)) {
-    $transactionStatus = 'Hash Verification Failure';
-    $success = false;
-}
+$systemurl = ($CONFIG['SystemSSLURL']) ? $CONFIG['SystemSSLURL'] . '/' : $CONFIG['SystemURL'] . '/';
 
-/**
- * Validate Callback Invoice ID.
- *
- * Checks invoice ID is a valid invoice number. Note it will count an
- * invoice in any status as valid.
- *
- * Performs a die upon encountering an invalid Invoice ID.
- *
- * Returns a normalised invoice ID.
- *
- * @param int $invoiceId Invoice ID
- * @param string $gatewayName Gateway Name
- */
+
+//  Validate Callback Invoice ID.
+
 $invoiceId = checkCbInvoiceID($invoiceId, $gatewayParams['name']);
 
-/**
- * Check Callback Transaction ID.
- *
- * Performs a check for any existing transactions with the same given
- * transaction number.
- *
- * Performs a die upon encountering a duplicate.
- *
- * @param string $transactionId Unique Transaction ID
- */
+
+//  Check Callback Transaction ID.
+
 checkCbTransID($transactionId);
 
-/**
- * Log Transaction.
- *
- * Add an entry to the Gateway Log for debugging purposes.
- *
- * The debug data can be a string or an array. In the case of an
- * array it will be
- *
- * @param string $gatewayName        Display label
- * @param string|array $debugData    Data to log
- * @param string $transactionStatus  Status
- */
-logTransaction($gatewayParams['name'], $_POST, $transactionStatus);
-
-if ($success) {
-
-    /**
-     * Add Invoice Payment.
-     *
-     * Applies a payment transaction entry to the given invoice ID.
-     *
-     * @param int $invoiceId         Invoice ID
-     * @param string $transactionId  Transaction ID
-     * @param float $paymentAmount   Amount paid (defaults to full balance)
-     * @param float $paymentFee      Payment fee (optional)
-     * @param string $gatewayModule  Gateway module name
-     */
+if ($status == "true") {
     addInvoicePayment(
         $invoiceId,
         $transactionId,
@@ -118,5 +54,13 @@ if ($success) {
         $paymentFee,
         $gatewayModuleName
     );
+    logTransaction($gatewayParams['name'], $_POST, "completed");
+    #redirect to invoice page
+    $invoice_url = $systemurl . 'viewinvoice.php?id=' . $invoiceId;
+    //header("Location: $invoice_url");
+    //exit;
+} elseif ($status == "false")
+    $values["status"] = "Failed";
+else
+    $values["status"] = "Unpaid";
 
-}
